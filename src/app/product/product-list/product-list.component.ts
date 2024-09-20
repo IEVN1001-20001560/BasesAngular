edfe5cc5a979b8c9d0597ccd522c4fedfe6382d1
Ceidy Martinez <ceidy117@gmail.com>
2024-09-19 22:10:34 -0600

    Segundo commit de BasesAngular 19-09-24

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -6,5 +6,47 @@ import { Component } from '@angular/core';
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
+  imageWidth:number=50;
+  imageMargin:number=2;
+  muestraImg:boolean=true;
+  mostrarImagen():void{
+    this.muestraImg=!this.muestraImg
+  }
 
+  productos:any[]=[{
+ 
+    "ProductoId":1,
+    "Modelo":"Sentra",
+    "Descripcion":"4 puertas",
+    "Year":"marzo 2021",
+    "Precio": 120000,
+    "Color": "Azul",
+    "Marca":"Chevrolet",
+    "ImagenUrl":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZLH2VtWDz4x24V4ldhNC_0IOxZaWAzxOdKg&s"
+ 
+  },
+ 
+  {
+    "ProductoId":2,
+    "Modelo":"Chevy",
+    "Descripcion":"4 puertas",
+    "Year":"enero 2021",
+    "Precio": 340000,
+    "Color": "Blanco",
+    "Marca":"NISSAN",
+    "ImagenUrl":"https://img.freepik.com/vector-gratis/coche-sedan-blanco-aislado-vector-blanco_53876-67356.jpg"
+ 
+  },
+ 
+  {
+    "ProductoId":3,
+    "Modelo":"Honda",
+    "Descripcion":"2 puertas",
+    "Year":"julio 2021",
+    "Precio": 220000,
+    "Color": "Rojo",
+    "Marca":"AUDI",
+    "ImagenUrl":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLR_kSYWcqhmxk7zHgcWqL0_aoB4DufpC1MA&s"
+  }
+  ]
 }
