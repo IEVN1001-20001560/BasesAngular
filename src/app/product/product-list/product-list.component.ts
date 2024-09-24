5edfb289c400ed56f07361abc91b7c887bba8e07
Ceidy Martinez <ceidy117@gmail.com>
2024-09-24 00:30:01 -0600

    tercer commit de basesAngular 23 de Septiembre 2024

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProductos } from '../productos';
 
 @Component({
   selector: 'app-product-list',
@@ -9,11 +10,13 @@ export class ProductListComponent {
   imageWidth:number=50;
   imageMargin:number=2;
   muestraImg:boolean=true;
+  listFiter:string='';
+
   mostrarImagen():void{
     this.muestraImg=!this.muestraImg
   }
 
-  productos:any[]=[{
+  productos: IProductos[]=[{
  
     "ProductoId":1,
     "Modelo":"Sentra",
@@ -23,7 +26,7 @@ export class ProductListComponent {
     "Color": "Azul",
     "Marca":"Chevrolet",
     "ImagenUrl":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZLH2VtWDz4x24V4ldhNC_0IOxZaWAzxOdKg&s"
- 
+
   },
  
   {
